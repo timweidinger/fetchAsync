36bd782530eec43c61151bead33ead1125ef68da
Tim Weidinger <timothy.weidinger@gmail.com>
2023-07-19 15:53:46 -0400

    Delete OSP/ExampleProjects/fetchAsync directory

--- OSP/ExampleProjects/fetchAsync/client/components/AsyncCall.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function AsyncCall() {
-  return <div>AsyncCall</div>;
-}
-
-export default AsyncCall;
--- OSP/ExampleProjects/fetchAsync/client/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const htmlRoot = ReactDOM.createRoot(document.getElementById('root'));
-
-htmlRoot.render(<App />);
